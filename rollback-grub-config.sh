53361ae9d4cb5e78614f5a7ebbe13599879c0531
Sujit <sujitfulse@gmail.com>
2015-10-23 13:57:42 +0530

    Create rollback-grub-config.sh

---
+++ rollback-grub-config.sh
@@ -0,0 +1,42 @@
+declare -a DATA
+index=0
+inc=1
+NEW=2
+OLD=1
+set -e
+fname="ostree-fedora-atomic-host-0.conf"
+fname1="ostree-fedora-atomic-host-1.conf"
+cd /boot/loader/entries
+for i in `ls -a *.*`
+do
+if [[ ("$i" == "$fname") || ("$i" == "$fname1") ]]; then
+echo "$i"
+
+while read -ra line;
+do
+    for word in "${line[@]}";
+    do
+       DATA[index]=`echo "$word"`;
+       index=$(expr "$index" + "$inc")
+    done;
+        done < "$i"
+        CURRENT=`echo ${DATA[16]}`;
+        echo ${DATA[15]} $CURRENT
+
+        if [ "$CURRENT" == "$OLD" ]; then
+           echo "No New Deploymnet found !!!"
+        fi
+
+        if [ "$CURRENT" == "$NEW" ]; then
+         echo "configuring bootconfig for new deployment";
+        echo -e "menuentry 'Fedora 22 (Twenty Two) Ostree' { \n
+        linux "${DATA[14]}" "${DATA[3]}" "${DATA[4]}"  "${DATA[5]}" \n
+        initrd "${DATA[1]}"
+        \n}" > /boot/loader/grub1.cfg
+        fi
+fi
+index=0
+done
+cd /boot/grub2/
+rm grub.cfg
+ln -s ../loader/grub1.cfg grub.cfg
